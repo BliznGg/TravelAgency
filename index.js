9bb456486563ddf7c261b72c5634df5109ab1b78
Artsiom <blizntema@gmail.com>
2024-06-23 19:11:57 +0300

    FIX(320 BP): sizes, form styles

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
 const burger = document.querySelector('.header__label')
-console.log(burger)
 
 const navigation = document.querySelector('.header__navigation')
 
 burger.addEventListener("click", myFunction);
 
 function myFunction() {
-    console.log('ky')
     document.body.classList.toggle('navigation-open')
 }
