4a98b6bdc094b42ae8e5f94270b1e47a4d18eae9
Artsiom <blizntema@gmail.com>
2024-07-27 19:14:42 +0300

    Fix (advantages.css, form.css) z-index for section \ form changes img btn

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-// const burger = document.querySelector('.header__label')
-//
-// const navigation = document.querySelector('.header__navigation')
-//
-// burger.addEventListener("click", myFunction);
-//
-// function myFunction() {
-//     document.body.classList.toggle('navigation-open')
-// }
+const burger = document.querySelector('.header__label')
+
+const navigation = document.querySelector('.header__navigation')
+
+burger.addEventListener("click", myFunction);
+
+function myFunction() {
+    document.body.classList.toggle('navigation-open')
+}
 
 
 // Задача.
